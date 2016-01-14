4bcd55bb48d63d9b0642dd9cceee3b772c4a7bf8
Slenders <lieven.slenders@wur.nl>
2016-01-14 12:18:40 +0100

    added polygons training

---
+++ main.R
@@ -0,0 +1,20 @@
+## Team Puma
+## Jason Davis & Lieven Slenders. 
+## January 13-1-2015
+# -------------------------------------------------------
+
+## Lesson 7
+
+## setting working directory
+getwd()
+# set working directory to you own specifications
+setwd("D:/workspace/geoscripting/lesson6")
+# 
+dir.create('data/')
+dir.create('R/')
+dir.create('output/')
+#---------------------------------------------------------------------------
+
+#tutorial: 
+
+
--- randomForest.R
+++ randomForest.R
@@ -30,23 +30,74 @@ head(rasterbrick)
 rasterbrickData <- getValues(rasterbrick)
 rasterbrickData <- as.data.frame(rasterbrick, na.rm=T)
 
-##plot
-pairs(rasterbrick)
 
 
-## Run linear regression model
-modelAll <- lm(VCF ~ band1+band2+band3+band4+band5+band7, rasterbrickData, na.action=na.omit)
+## relationship between the Landsat bands and the VCF tree cover--------------
+#plot pairs to observe correlations and to select bands for further analysis
+pairs(brick(rasterbrick$band1,rasterbrick$band2,rasterbrick$band3,rasterbrick$VCF))
+pairs(brick(rasterbrick$band4,rasterbrick$band5,rasterbrick$band7,rasterbrick$VCF))
+pairs(brick(rasterbrick$band1,rasterbrick$band7,rasterbrick$ndvi,rasterbrick$VCF))
+
+# Run linear regression model with selected bands
+modelAll <- lm(VCF ~ band1+band2+band3+band4+band5+band7+ndvi, rasterbrickData, na.action=na.omit)
 summary(modelAll)
-plot(modelAll)
-  
-  
-  
-  
-# load the training polygons -------------------------------
-load("AdvancedRasterAnalysis-gh-pages/data/trainingPoly.rda")
 
-# we can convert to integer by using the as.numeric() function, 
-# which takes the factor levels
+# predictors (bands) probably most important 
+mode357ndvi <- lm(VCF ~ band3+band5+band7, rasterbrickData, na.action=na.omit)
+summary(mode357ndvi)
+
+## plot the predicted tree cover raster ------------------------------------
+# load the training polygons 
+load("AdvancedRasterAnalysis-gh-pages/data/trainingPoly.rda")
+# superimpose training polygons onto ndvi plot
+plot(ndvi)
+plot(trainingPoly, add = TRUE)
+
+#'relabel' our training classes
 trainingPoly@data$Code <- as.numeric(trainingPoly@data$Class)
-trainingPoly@data
+
+# assign 'Code' values to raster cells (where they overlap)
+classes <- rasterize(trainingPoly, ndvi, field='Code')
+# colors to classes
+cols <- c("orange", "dark green", "light blue")
+
+# plot without a legend
+plot(classes, col=cols, legend=FALSE)
+
+# add a customized legend
+legend("topright", legend=c("cropland", "forest", "wetland"), fill=cols, bg="white")
+
+##
+covBrick <- brick(rasterbrick$band3, rasterbrick$band5, rasterbrick$band7, rasterbrick$ndvi, rasterbrick$VCF)
+names(covBrick) <- c("band3", "band5", "band7", "ndvi", "VCF")
+## Mask band images by polygons
+covmasked <- mask(covBrick, classes)
+
+# combine this new brick with the classes layer to make our input training dataset
+names(classes) <- "class"
+covBrick <- addLayer(covmasked, classes)
+plot(covBrick)
+
+# extract all values into a matrix and remove NA values from valuetable
+valuetable <- getValues(covBrick)
+valuetable <- na.omit(valuetable)
+
+##Convert values to data frame
+df_valuetable <- as.data.frame(valuetable)
+range(df_valuetable$class)
+
+
+df_valuetable$class <- factor(df_valuetable$class, levels = c(1:3)) ## must WHY ??? 
+
+val_crop <- subset(df_valuetable, class == 1)
+val_forest <- subset(df_valuetable, class == 2)
+val_wetland <- subset(df_valuetable, class == 3)
+
+
+
+# compare with the original VCF raster 
+..
+
+
+
 
